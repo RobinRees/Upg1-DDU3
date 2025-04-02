e5c00a6b1bfc87463458999935595d58e010cae9
RobinRees <Reehees12@gmail.com>
2025-04-02 10:42:06 +0200

    sfdggh

---
+++ DDU3/Tisdag V14/Tisdag/övningar/ö4 - SameServer/filerna_som_ska_servas/index.js
@@ -0,0 +1,15 @@
+const options = {
+  method: "GET",
+};
+
+const request = new Request("http://localhost:4242/random", options);
+
+fetch(request)
+  .then(x => x.text())
+  .then(handleResource);
+
+function handleResource (resource) {
+  document.body.style.backgroundColor = resource;
+  document.querySelector("#color").textContent = resource;
+}
+
