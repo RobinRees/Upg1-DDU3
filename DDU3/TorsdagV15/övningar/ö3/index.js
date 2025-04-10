6a70d72e5e4988491ee7936b7a7b0c78cdb1e095
RobinRees <Reehees12@gmail.com>
2025-04-10 15:08:25 +0200

    sdf
    fds

---
+++ DDU3/TorsdagV15/övningar/ö3/index.js
@@ -0,0 +1,71 @@
+const registerButton = document.getElementById("registerButton")
+const registerInputName = document.getElementById("registerInputName")
+const registerInputPw = document.getElementById("registerInputPw")
+
+const loginButton = document.getElementById("loginButton")
+const loginInputName = document.getElementById("loginInputName")
+const loginInputPw = document.getElementById("loginInputPw")
+
+const responseMessage = document.getElementById("textResponse")
+
+registerButton.addEventListener("click", async function(){
+
+
+    if (registerInputName.value != "" && registerInputPw.value != ""){
+        const request = new Request("https://maumt.reipino.com/du3/servers/accounts/manage/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({name: `${registerInputName.value}`, password: `${registerInputPw.value}`})
+        });
+    
+        (async function () {
+            await fetch(request);
+            let response = await fetch("https://maumt.reipino.com/du3/servers/accounts/");
+            let accounts = await response.json();
+            console.log(accounts);
+        })();
+    
+    
+        console.log(accounts)
+
+
+    } else {
+        responseMessage.innerHTML = "Was not registered"
+    }
+
+})
+
+
+loginButton.addEventListener("click", async function() {
+    if (registerInputName.value != "" && registerInputPw != "") {
+
+
+        const request = new Request("https://maumt.reipino.com/du3/servers/accounts/login/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({name: `${loginInputName.value}`, password: `${loginInputPw.value}`})
+        });        
+
+
+        const responsePromise = fetch(request);
+        responsePromise.then(response => {
+            if (response.status === 200) {
+                responseMessage.innerHTML = "Du har loggat in";
+            } else {
+                responseMessage.innerHTML = "Ditt lösenord Funkar inte"
+            }
+
+        })
+
+
+
+    } else {
+        responseMessage.innerHTML = "Det funkar inte"
+    }
+
+
+})
